c86c578f7fb251ed16fcf5d2bc49107b33d51998
dftortosa <dsalazar@ugr.es>
2025-01-04 14:21:37 +0100

    updating instructions of the program

--- scripts/01_step_2.py
+++ scripts/01_step_2.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3.10
 # coding: utf-8
     #to run this script: chmod +x script.py; ./script.py
-    #!/bin/sh does not work with my terminal en msi of David.
     #if you are using "$" to paste the path of the executable, you do not need to use "./" for running the executable.
     #you can save the output and the errors
         #./script.py > script.out #only output
         #./script.py 2> error.out #only error
         #./script.py > script.out 2> error.out #both in different files
         #./script.py > script.out 2>&1 #both in the same file
-        #https://www.cyberciti.biz/faq/linux-redirect-error-output-to-file/
 
 
 ##function to input expenses
